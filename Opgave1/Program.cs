03ace7cb1a0e6de9e85485b1218b15d9e4497218
Daniel Spurrell <110387670+Danman05@users.noreply.github.com>
2023-04-17 13:26:10 +0200

    Add files via upload

---
+++ Opgave1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Øvelse 0 - C# programmering trådpool øvelser
+*/
+
+namespace Opgave1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Program prg = new Program();
+
+            // Loop that repeats 2 times. There are created 2 threads in a ThreadPool for both methods
+            for (int i = 0; i < 2; i++)
+            {
+                ThreadPool.QueueUserWorkItem(prg.Task1);
+                ThreadPool.QueueUserWorkItem(prg.Task2);
+            }
+
+            Console.Read();
+        }
+
+        // Method that is accessed by the threads in the ThreadPool from main
+        public void Task1(object obj) 
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Task 1 is being executed");
+            }
+        }
+
+        // Method that is accessed by the threads in the ThreadPool from main
+
+        public void Task2(object obj)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Task 2 is being executed");
+            }
+        }
+
+
+    }
+}
---
+++ Opgave2/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Øvelse 1 - C# programmering trådpool øvelser
+*/
+using System.Diagnostics;
+using System.Threading;
+
+namespace Opgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates a new instance of stopwatch
+            Stopwatch sw = new Stopwatch();
+
+
+            // using stopwatch to take time of the elapsed time from the execution of ThreadPoolMethod and ThreadMethod
+            Console.WriteLine("Thread Pool Execution");
+            sw.Start();
+            ProcessWithThreadPoolMethod();
+            sw.Stop();
+            Console.WriteLine($"Time consumed by ThreadPoolMethod is: {sw.ElapsedTicks}\n");
+
+            sw.Reset();
+
+            Console.WriteLine("Thread Execution");
+            sw.Start();
+            ProcessWithThreadMethod();
+            sw.Stop();
+            Console.WriteLine($"Time consumed by ThreadMethod is: {sw.ElapsedTicks}");
+
+            Console.Read();
+        }
+        // 
+        static void Process(object obj)
+        {
+            /*
+             * 1 for-loop gives high execution time - ThreadPool is fast
+             * 2 for-loops gives fast execution time for ThreadPoolMethod while ThreadMethod is much slower
+             */
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int n = 0; n < 100000; n++)
+                {
+                }
+            }
+        }
+        /// <summary>
+        /// Method that uses ThreadPool to execute the Process method
+        /// </summary>
+        static void ProcessWithThreadPoolMethod()
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+        /// <summary>
+        /// Method that uses Thread to execute the Process method
+        /// </summary>
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+    }
+}
